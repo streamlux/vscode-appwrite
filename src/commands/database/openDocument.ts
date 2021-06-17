87c0d61ed3890b21c9421688b83f4e15fb1344c5
alexweininger <alex.weininger@live.com>
2021-06-17 16:07:34 -0700

    Add setting to format json strings when viewing a document

--- src/commands/database/openDocument.ts
+++ src/commands/database/openDocument.ts
@@ -1,8 +1,30 @@
+import { workspace } from "vscode";
 import { DocumentTreeItem } from "../../tree/database/DocumentTreeItem";
 import { openReadOnlyJson } from "../../ui/openReadonlyContent";
 
+function parseJSONString(str: string): { valid: boolean; value: any } {
+    try {
+        return { value: JSON.parse(str), valid: true };
+    } catch (e) {
+        return { value: str, valid: false };
+    }
+}
+
 export async function viewDocumentAsJson(documentTreeItem: DocumentTreeItem): Promise<void> {
-    const documentId = documentTreeItem.document["$id"];
+    const document = documentTreeItem.document;
+    const documentId = document["$id"];
+
+    const formatJsonStrings = workspace.getConfiguration("appwrite").get<Boolean>("formatJsonStrings");
+
+    if (formatJsonStrings) {
+        Object.entries(document).forEach(([key, value]) => {
+            if (typeof value === "string") {
+                const result = parseJSONString(value);
+                document[key] = result;
+            }
+        });
+    }
+
     await openReadOnlyJson(
         {
             label: documentId,
